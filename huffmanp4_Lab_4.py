4864ad2b5e24284f6158752298de0b9a15e7d7c9
Paul <morningperson2@gmail.com>
2022-09-28 13:39:26 -0400

    comments and such

--- huffmanp4_Lab_4.py
+++ huffmanp4_Lab_4.py
@@ -1,10 +1,26 @@
+################################################################################
+#
+# File        huffmanp4_Lab4.py
+# Author      Paul Huffman
+# Email:      huffmanp4@nku.edu
+# Course:     DSC200
+# Section:    001
+# Assignment: 4
+# Date:       9/28/2022
+# Brief:
+#  This file contains the implementation of the prompts for Lab 4
+#  
+#  Hours spent on this assignment: 3
+#
+################################################################################
 import openpyxl as oxl
 from csv import writer
 
+# This loads the file, and assigns it to the variable myWorkbook
 myWorkbook = oxl.load_workbook("Lab4Data.xlsx", read_only=True, keep_vba=False, data_only=True)
 
+# This is making a dictionary of possible catagory titles
 header = ['CountryName', 'CategoryName', 'CategoryTotal']
-
 catNames = {
     3: "child_labor_total",
     5: "child_labor_male",
@@ -23,23 +39,39 @@ catNames = {
 }
 data = []
 
+
 wsht = myWorkbook.active
 ctrTotal = 1
 
+# This loops through the active worksheet to pull the necessary data
 for row in wsht.iter_rows(min_col = 2, max_col = 32, min_row = 15, max_row = 211):
     colCntr = 0
     for cell in row:
         if colCntr == 0:
             countryName = cell.value
-        elif colCntr > 2 and colCntr < 30 and colCntr % 2 == 1 and cell.value != '\u2013' and cell.value != "\u2013 ":
+            colCntr += 1
+        elif colCntr < 2 or colCntr > 30 :
+            colCntr += 1
+            continue
+        elif colCntr % 2 == 0 :
+            colCntr += 1
+            continue
+        elif cell.value == '\u2013' :
+            colCntr += 1
+            continue
+        elif cell.value == "\u2013 ":
+            colCntr += 1
+            continue
+        else:
             catName = catNames[colCntr]
             value = cell.value
             data.append([countryName,catName,value])
             ctrTotal += 1
+            colCntr += 1
         
-        colCntr += 1
+       
 
-
+# This writes the data to a csv file, then prints the total number of lines printed
 with open("huffmanp4_sample.csv", "w", newline='') as wfileObj:
         mwriter = writer(wfileObj)
 
